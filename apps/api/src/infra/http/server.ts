c9429fb07dcf7e2639e201f859210b32034d483c
DouglasMai4 <douglasmai4@protonmail.com>
2026-01-08 16:45:41 -0300

    feat(http): Setup express server with cors config and dotenv

--- apps/api/src/infra/http/server.ts
+++ apps/api/src/infra/http/server.ts
@@ -1,20 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 
+import { config } from 'dotenv';
+
+config({
+	quiet: true,
+});
+
+const PORT = parseInt(process.env.PORT || '3030', 10);
+
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGINS?.split(',') || '*',
+	}),
+);
 
 app.get('/', (_req, res) => {
-  res.json({
-    sucess: true,
-    data: {
-      message: 'Hello Goalfy!'
-    }
-  })
+	res.json({
+		sucess: true,
+		data: {
+			message: 'Hello Goalfy!',
+		},
+	});
 });
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:${3000}`);
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${3000}`);
 });
